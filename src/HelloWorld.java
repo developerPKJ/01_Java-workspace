2e41adfbabe544a0ac0b27e00c7484bac1900213
박경준 <siteblod@gmail.com>
2026-02-04 17:42:05 +0900

    26_02_04

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+// 디폴트 패키지는 패키지 선언부가 존재하지 않음(기본 패키지)
+// 클래스 생성 및 소스코드 작업이 가능하긴 하나, 추후 다른 클래스에서 상속이 불가능함(import) - 위치 모름
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/kh/second/MethodTest.java
@@ -0,0 +1,17 @@
+package com.kh.second;
+
+public class MethodTest {
+
+	static void a() {
+		System.out.println("A"); 
+		System.out.println("a"); 
+	}
+	public static void b() {
+		System.out.println("B");
+		a();
+	}
+	public static void c() {
+		System.out.println("C");
+		b();
+	}
+}
