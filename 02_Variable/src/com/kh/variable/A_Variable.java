279e97569c817cbdf3d11bcea89f69f6c7c1fc52
박경준 <siteblod@gmail.com>
2026-02-05 11:42:26 +0900

    26_02_05 (2)
    gitignore

---
+++ 02_Variable/src/com/kh/variable/A_Variable.java
@@ -0,0 +1,13 @@
+package com.kh.variable;
+
+public class A_Variable {
+	
+	public void printSquare() {
+		int a = 6;
+		int b = 7;
+		
+		System.out.println("넓이 :"+ a*b);
+		System.out.println("둘레 :"+ (a+b)*2);
+	}
+
+}
---
+++ 02_Variable/src/com/kh/variable/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.variable.run;
+
+import com.kh.variable.A_Variable;
+
+public class Run {
+
+	public static void main(String[] args) {
+		A_Variable av = new A_Variable();
+		av.printSquare();
+	}
+	
+}
